d9b1096c8117cc057e2a6f6d2e00fac1c9a0a117
Paul Cluzel <pcluzel@pcalb-mm0000.univ-jfc.fr>
2023-03-15 10:01:09 +0100

    remplissage constructeur de profil

--- Champolove/src/Profil.java
+++ Champolove/src/Profil.java
@@ -3,18 +3,28 @@ public class Profil {
 	
 	String nom;
 	String prenom;
-	Integer age;
+	int age;
 	String sexe;
 	String orientation_sexuelle;
 	String lien_photoProfil;
 	String description;
 	
 	ArrayList<String> Gouts;
-	ArrayList<String> Hobbies
+	ArrayList<String> Hobbies;
 	ArrayList<String> aEviter;
 	
-	public Profil(String nom, String prenom, Integer age, String sexe, String orienta_sex, String lien_photoP, String descri) {
+	public Profil(String nom, String prenom, int age, String sexe, String orienta_sex, String lien_photoP, String descri) {
+		this.nom=nom;
+		this.prenom=prenom;
+		this.age=age;
+		this.sexe=sexe;
+		this.orientation_sexuelle=orienta_sex;
+		this.lien_photoProfil=lien_photoP;
+		this.description=descri;
 		
+		ArrayList<String> Gouts=ArrayList<>();
+		ArrayList<String> Hobbies=ArrayList<>();
+		ArrayList<String> aEviter=ArrayList<>();
 	}
 	
 }
