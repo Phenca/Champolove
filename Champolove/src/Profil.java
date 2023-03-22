95bf2f0e5be6f3e564d158bef2b0b9468f267043
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-03-22 08:56:58 +0100

    changement mineur dans le constructeur

--- Champolove/src/Profil.java
+++ Champolove/src/Profil.java
@@ -5,6 +5,7 @@ public class Profil {
 	String identifiant;
 	String nom;
 	String prenom;
+	int taille;
 	int age;
 	String adresse_mail;
 	String sexe;
@@ -12,14 +13,16 @@ public class Profil {
 	String lien_photoProfil;
 	String description;
 	
-	ArrayList<String> Gouts; // question sur la musique, question sur la nourriture
-	ArrayList<String> Hobbies;
-	ArrayList<String> aEviter;
+	ArrayList<String> Gouts; // question sur la musique, question sur la nourriture, choix âge, choix taille, choix sexe
+	ArrayList<String> Hobbies; // Sport ? Jeux ? Autre Activité ?
+	ArrayList<String> aEviter; // Allergie ?
 	
-	public Profil(String identifiant, String nom, String prenom, int age, String adresse_mail, String sexe, String orienta_sex, String lien_photoP, String descri) {
+	public Profil(String id, String nom, String prenom, int age, String adresse_mail, String sexe, String orienta_sex, String lien_photoP, String descri) {
+		this.identifiant= id;
 		this.nom=nom;
 		this.prenom=prenom;
 		this.age=age;
+		this.adresse_mail = adresse_mail;
 		this.sexe=sexe;
 		this.orientation_sexuelle=orienta_sex;
 		this.lien_photoProfil=lien_photoP;
@@ -29,5 +32,4 @@ public class Profil {
 		ArrayList<String> Hobbies= new ArrayList<>();
 		ArrayList<String> aEviter= new ArrayList<>();
 	}
-	
 }
