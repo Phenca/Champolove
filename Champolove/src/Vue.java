89b3aa79d963c93509292952aef60673cfbb8a9d
Phenca <soran@LORDI-51QDLSFOO.localdomain>
2023-03-25 15:48:11 +0100

    Création page d'accueil (style a faire plus tard)

--- Champolove/src/Vue.java
+++ Champolove/src/Vue.java
@@ -1,27 +1,40 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 
 public class Vue extends Application {
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setWidth(800);
-		primaryStage.setHeight(800);
-		primaryStage.setTitle("Champolove");
+	public void start(Stage Stage) throws Exception {
+		Label title = new Label("Champolove");
+		title.relocate(110, 50);
+
+		Label lblLog = new Label("Déjà inscrit ?");
+		lblLog.relocate(110, 150);
+		Button loginBtn = new Button("Login");
+		loginBtn.relocate(120, 175);
 		
-		Group fenetre = new Group();
+		Label lblSign = new Label("Pas encore inscrit ?");
+		lblSign.relocate(110, 225);
+		Button signinBtn = new Button("Sign in");
+		signinBtn.relocate(120, 250);
+		
+		Pane fenetre = new Pane();
+		fenetre.getChildren().addAll(title, lblLog, loginBtn, lblSign, signinBtn);	
+		
+		fenetre.setPrefSize(300, 500);
+		
 		Scene scene = new Scene(fenetre);
-		scene.setFill(Color.SKYBLUE);
-		
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		Stage.setScene(scene);
+		Stage.setTitle("Champolove");
+		//Stage.getIcons().add(new Image(getClass().getResourceAsStream("logo.png")));
+		Stage.show();
 		
 	}
 	public static void main(String[] args) {
 		launch(args);
 	}
-
-
 }
