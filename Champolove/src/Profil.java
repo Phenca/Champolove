cc8084feb56d314af3985079499b9c563224cb42
Kappa2727 <paulau.simbae@gmail.com>
2023-03-28 21:53:10 +0200

    creation du compareTo au Profil.java

--- Champolove/src/Profil.java
+++ Champolove/src/Profil.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Profil {
-	
+
 	String identifiant;
 	String nom;
 	String prenom;
@@ -9,7 +9,6 @@ public class Profil {
 	int age;
 	String adresse_mail;
 	String sexe;
-	String orientation_sexuelle;
 	String lien_photoProfil;
 	String description;
 
@@ -17,8 +16,8 @@ public class Profil {
 	ArrayList<String> Hobbies; // Sport ? Jeux ? Autre Activité ?
 	ArrayList<String> aEviter; // Allergie ?
 
-	
-	public Profil(String id, String nom, String prenom, int age,int taille, String adresse_mail, String sexe, String orienta_sex, String lien_photoP, String descri) {
+
+	public Profil(String id, String nom, String prenom, int age,int taille, String adresse_mail, String sexe, String lien_photoP, String descri) {
 		this.identifiant= id;
 		this.nom=nom;
 		this.prenom=prenom;
@@ -26,17 +25,53 @@ public class Profil {
 		this.taille=taille;
 		this.adresse_mail = adresse_mail;
 		this.sexe=sexe;
-		this.orientation_sexuelle=orienta_sex;
 		this.lien_photoProfil=lien_photoP;
 		this.description=descri;
-		
+
 		ArrayList<String> Gouts= new ArrayList<>();
 		ArrayList<String> Hobbies= new ArrayList<>();
 		ArrayList<String> aEviter= new ArrayList<>();
 	}
-	
-	
-	public Object compareTo(Object o) {
-		return null;
+
+
+	public int compareTo(Profil o) {
+		int res=0;
+		int totalDesGouts=0;
+		int totalDesHobbies=0;
+		int totalDesTrucAEVITER=0;
+
+
+		for(int i=0; i<this.Gouts.size(); i++) {
+			for(int j=0; j<o.Gouts.size(); j++) {
+				if(this.Gouts.get(i)==o.Gouts.get(j)) {
+					res+=1;
+				}
+			}
+		}
+		totalDesGouts=(this.Gouts.size()+o.Gouts.size());
+		
+		
+		for(int i=0; i<this.Hobbies.size(); i++) {
+			for(int j=0; j<o.Hobbies.size(); j++) {
+				if(this.Hobbies.get(i)==o.Hobbies.get(j)) {
+					res+=1;
+				}
+			}
+		}
+		
+		totalDesHobbies=(this.Hobbies.size()+o.Hobbies.size());
+		
+		for(int i=0; i<this.aEviter.size(); i++) {
+			for(int j=0; j<o.aEviter.size(); j++) {
+				if(this.aEviter.get(i)==o.aEviter.get(j)) {
+					res+=1;
+				}
+			}
+		}
+		
+		totalDesTrucAEVITER=(this.aEviter.size()+o.aEviter.size());
+
+
+		return res/((totalDesGouts + totalDesHobbies + totalDesTrucAEVITER) - res); ///ça donne un pourcentage, ce qu'il faut comprendre c'est que le deuxième res qui est a l extreminite sert juste a eleminer les doublons des points communs dans les totals de chaque gouts, hobbies et aEviter
 	}
 }
