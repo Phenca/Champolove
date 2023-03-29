0755b715d1e69dc83001423af8fcc575c3a4ddb5
Kappa2727 <paulau.simbae@gmail.com>
2023-03-29 11:02:43 +0200

    truc

--- Champolove/src/Controleur.java
+++ Champolove/src/Controleur.java
@@ -1,11 +1,18 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
-
-public class Controleur {
+public class Controleur implements EventHandler<ActionEvent>{
 	
 	private Modele modl;
 	
 	public Controleur (Modele modl) {
 		this.modl=modl;
+	}
+
+	@Override
+	public void handle(ActionEvent event) {
+		System.out.println(event.getTarget());
+		
 	}
 	
 	
--- Champolove/src/Vue.java
+++ Champolove/src/Vue.java
@@ -3,10 +3,17 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Vue extends Application implements Observer{
+
 	@Override
 	public void start(Stage Stage) throws Exception {
+
 		pageAcceuil launchPage = new pageAcceuil();
 		launchPage.start(Stage);
+
+		
+		Modele m=new Modele();
+		Controleur c=new Controleur(m);
+
 	}
 	public static void main(String[] args) {
 		launch(args);
