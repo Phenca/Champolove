dbacaaf43fccb6828945e81991e5ed20bc5e61dc
Kappa2727 <paulau.simbae@gmail.com>
2023-04-25 15:08:10 +0200

    changement des noms de la mémoire

--- Champolove/src/application/MemoireProfil.java
+++ Champolove/src/application/MemoireProfil.java
@@ -7,10 +7,10 @@ public class MemoireProfil {
 	
 	int age = 0;
 	ArrayList<Profil> memoirePuser;
-	ArrayList<Profil> memoirePuserHomme;
-	ArrayList<Profil> memoirePuserFemme;
+	ArrayList<Profil> memoirePHomme;
+	ArrayList<Profil> memoirePFemme;
 	Profil pUser;
-	ArrayList<Profil> memoireP;
+	ArrayList<Profil> memoirePMixtes;
 	Profil p1;
 	Profil p2;
 	Profil p3;
@@ -23,7 +23,7 @@ public class MemoireProfil {
 	Profil p10;
 
 	
-	public static int calcul_age(ArrayList memoireP, int newage) {
+	public static int calcul_age(ArrayList<Profil> memoireP, int newage) {
 		Calendar c = Calendar. getInstance();
 		for (int i=0; i<memoireP.size(); i++) {
 			Profil m =  (Profil) memoireP.get(i);
@@ -59,40 +59,40 @@ public class MemoireProfil {
 		memoirePuser= new ArrayList<>();
 		pUser = new Profil(null, null, null, null, 0, age , null, null, null, null, 0, null);
 		memoirePuser.add(pUser);
-		memoireP = new ArrayList<>();
+		memoirePMixtes = new ArrayList<>();
 		p1 = new Profil("p1", "Levy", "Adolphe", "04/03/2002", 179, age, "Adolphe.Levy@yopmail.com", "masculin", "file:ressources/img_profil/Levy_Adolphe.png", "", 0,"courgette1");
-		memoireP.add(p1);
+		memoirePMixtes.add(p1);
 		p2 = new Profil("p2", "Hamel", "Bertrand", "24/01/1983", 183, age, "Bertrand.Hamel@yopmail.com", "masculin", "file:ressources/img_profil/Hamel_Bertrand.png", "", 0,"courgette2");
-		memoireP.add(p2);
+		memoirePMixtes.add(p2);
 		p3 = new Profil("p3", "Hardy", "Rochelle", "03/11/1997", 165, age, "Rochelle.Hardy@yopmail.com", "féminin", "file:ressources/img_profil/Hardy_Rochelle.png", "", 0,"courgette3");
-		memoireP.add(p3);
+		memoirePMixtes.add(p3);
 		p4 = new Profil("p4", "Thomas", "Antoine", "10/09/2004", 170, age, "Antoine.Thomas@yopmail.com", "masculin", "file:ressources/img_profil/Thomas_Antoine.png", "", 0,"courgette4");
-		memoireP.add(p4);
+		memoirePMixtes.add(p4);
 		p5 = new Profil("p5", "Fort", "Lucy", "13/02/1999", 175, age, "Lucy.Fort@yopmail.com", "féminin", "file:ressources/img_profil/Fort_Lucy.png", "", 0,"courgette5");
-		memoireP.add(p5);
+		memoirePMixtes.add(p5);
 		p6 = new Profil("p6", "Andre", "Margot", "20/02/1992", 166, age, "Margot.Andre@yopmail", "féminin", "file:ressources/img_profil/Andre_Margot.png", "", 0,"courgette6");
-		memoireP.add(p6);
+		memoirePMixtes.add(p6);
 		p7 = new Profil("p7", "Drouet", "Charlie", "16/03/1965", 166, age, "Charlie.Drouet@yopmail", "masculin", "file:ressources/img_profil/Drouet_Charlie.png", "", 0,"courgette7");
-		memoireP.add(p7);
+		memoirePMixtes.add(p7);
 		p8 = new Profil("p8", "Grandjean", "Michelle", "29/12/1966", 157, age, "Michelle.Grandjean@yopmail.com", "féminin", "file:ressources/img_profil/Grandjean_Michelle.png", "", 0,"courgette8");
-		memoireP.add(p8);
+		memoirePMixtes.add(p8);
 		p9 = new Profil("p9", "Dupuis", "Lambert", "06/04/1967", 174, age, "Lambert.Dupuis@yopmail.com", "masculin", "file:ressources/img_profil/Dupuis_Lambert.png", "", 0,"courgette9");
-		memoireP.add(p9);
+		memoirePMixtes.add(p9);
 		p10 = new Profil("p10", "Pasquier", "Colette", "01/01/1978", 167, age, "Colette.Pasquier@yopmail.com", "féminin", "file:ressources/img_profil/Pasquier_Colette.png", "", 0,"courgette10");
-		memoireP.add(p10);
+		memoirePMixtes.add(p10);
 		
-		calcul_age(memoireP, age);
+		calcul_age(memoirePMixtes, age);
 		
-		memoirePuserHomme = new ArrayList<>();
-		memoirePuserFemme = new ArrayList<>();
+		memoirePHomme = new ArrayList<>();
+		memoirePFemme = new ArrayList<>();
 		
-		for (int i=0; i<memoireP.size(); i++) {
-			Profil m =  (Profil) memoireP.get(i);
+		for (int i=0; i<memoirePMixtes.size(); i++) {
+			Profil m =  (Profil) memoirePMixtes.get(i);
 			if (m.sexe == "masculin"){
-				memoirePuserHomme.add(m);
+				memoirePHomme.add(m);
 			}
 			else{
-				memoirePuserFemme.add(m);
+				memoirePFemme.add(m);
 			}
 		}
 
