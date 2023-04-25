db47f313a4da2b30a2aa407a09264136d429a353
Kappa2727 <paulau.simbae@gmail.com>
2023-04-25 14:57:48 +0200

    suppression de la liste aeviter dans la classe profil

--- Champolove/src/application/Profil.java
+++ Champolove/src/application/Profil.java
@@ -17,7 +17,6 @@ public class Profil {
 
 	public ArrayList<String> Gouts; // question sur la musique, question sur la nourriture, choix taille
 	ArrayList<String> Hobbies; // Sport ? Jeux ? Autre Activité ?
-	ArrayList<String> aEviter; // Allergie ?
 
 
 	public Profil(String id, String nom, String prenom, String date_naissance,int age, int taille, String adresse_mail, String sexe, String lien_photoP, String descri, int matcher, String motsDePasse) {
@@ -33,9 +32,8 @@ public class Profil {
 		this.matcher = 0;
 		this.mdp=motsDePasse;
 		
-		ArrayList<String> Gouts= new ArrayList<>();
-		ArrayList<String> Hobbies= new ArrayList<>();
-		ArrayList<String> aEviter= new ArrayList<>();
+		Gouts= new ArrayList<>();
+		Hobbies= new ArrayList<>();
 	}
 
 
@@ -43,7 +41,6 @@ public class Profil {
 		int res=0;
 		int totalDesGouts=0;
 		int totalDesHobbies=0;
-		int totalDesTrucAEVITER=0;
 
 
 		for(int i=0; i<this.Gouts.size(); i++) {
@@ -65,18 +62,8 @@ public class Profil {
 		}
 		
 		totalDesHobbies=(this.Hobbies.size()+o.Hobbies.size());
-		
-		for(int i=0; i<this.aEviter.size(); i++) {
-			for(int j=0; j<o.aEviter.size(); j++) {
-				if(this.aEviter.get(i)==o.aEviter.get(j)) {
-					res+=1;
-				}
-			}
-		}
-		
-		totalDesTrucAEVITER=(this.aEviter.size()+o.aEviter.size());
 
 
-		return res/((totalDesGouts + totalDesHobbies + totalDesTrucAEVITER) - res); ///ça donne un pourcentage, ce qu'il faut comprendre c'est que le deuxième res qui est a l extreminite sert juste a eleminer les doublons des points communs dans les totals de chaque gouts, hobbies et aEviter
+		return res/((totalDesGouts + totalDesHobbies) - res); ///ça donne un pourcentage, ce qu'il faut comprendre c'est que le deuxième res qui est a l extreminite sert juste a eleminer les doublons des points communs dans les totals de chaque gouts, hobbies et aEviter
 	}
 }
